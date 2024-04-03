76e04b38c923ae2fededebea0e6bface90ec14b7
Sean <seancapelle@gmail.com>
2024-04-03 11:44:04 -0400

    Initial project setup

---
+++ SWUdeckbuilder/src/main/java/com/mycompany/swudeckbuilder/Deck.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.swudeckbuilder;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author seanc
+ */
+public class Deck {
+    private ArrayList<Card> deck = new ArrayList<>();
+    
+    // addCard
+    
+    // removeCard
+    
+    // getCards
+    
+    // shuffle 
+    
+    // drawHand
+    
+    // draw
+    
+    // discard?
+    
+//    System.out.println(deck.toString());
+}
---
+++ SWUdeckbuilder/src/main/java/com/mycompany/swudeckbuilder/SWUdeckbuilder.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.swudeckbuilder;
+
+/**
+ *
+ * @author seanc
+ */
+public class SWUdeckbuilder {
+
+    public static void main(String[] args) {
+        User sean = new User("Sean");
+        System.out.println(sean.getName());
+    }
+}
